3452548b68a5011d5051c136c52282217ed944a9
AndreaLK3 <andrea.lekkas@outlook.com>
2019-08-01 11:03:46 +0200

    Wiktionary definitions

--- GetDefinitions/Wiktionary.py
+++ GetDefinitions/Wiktionary.py
@@ -2,35 +2,43 @@ import requests
 import Utils
 import logging
 import wiktionaryparser as WP
+import re
 
-URL = "https://en.wiktionary.org/w/api.php"
+#URL = "https://en.wiktionary.org/w/api.php"
 
-def parse_for_word(target_word, session):
-    PARAMS = {
-        'action': "parse",
-        'page': target_word,
-        'format': "json"
-    }
+def process_defs_subdicts(structure_ls):
 
-    R = session.get(url=URL, params=PARAMS)
-    data = R.json()
-    return data
+    defs = []
+    examples = []
+
+    for pos_dict in structure_ls:
+        defs.extend(pos_dict['text'][1:]) # skip [0] as it is grammar, not a definition
+        examples.extend(pos_dict['examples'])
+
+    return (defs, examples)
+
+
+# Wiktionary definition often start with a specification, like (transitive), (obsolete), (botany), (uncountable).
+# It should be removed.
+def clean_definitions(defs_ls):
+    new_defs = list(map(lambda definition: re.sub("^\([a-zA-Z0-9_\s,]+\)" , "", definition) , defs_ls))
+    return new_defs
 
 
 def main():
     Utils.init_logging('Wiktionary.log', logging.INFO)
 
-    #S = requests.Session()
     target_word = "plant"
-
-    #word_data = parse_for_word(target_word, S)
 
     parser = WP.WiktionaryParser()
     word_data = parser.fetch(target_word)
     logging.debug(word_data)
 
-    defs_structure = word_data['definitions']
+    defs_structure = word_data[0]['definitions']
+    (raw_definitions, _examples) = process_defs_subdicts(defs_structure)
+    definitions = clean_definitions(raw_definitions)
 
-  
+    logging.info(definitions)
+
 
 main()
