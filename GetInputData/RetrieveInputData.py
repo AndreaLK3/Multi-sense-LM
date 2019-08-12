a13286ac2f865e5981222760281f32cb6477e94f
Andrea Lekkas <anl@ordbogen.com>
2019-08-12 18:31:08 +0200

    added data source and target word to storage...
    Former-commit-id: 4e1d5ececdf4597d0f8d001f3cb95b034246d18e

--- GetInputData/RetrieveInputData.py
+++ GetInputData/RetrieveInputData.py
@@ -8,26 +8,32 @@ import OmegaWiki
 import pandas as pd
 import os
 import time
+from itertools import cycle
+
 
 NUM_WORDS_IN_FILE = 5000
 HDF5_BASE_CHARSIZE = 1024
 
 
-def refine_synonyms(synonyms_ls, target_word, exclude_multiword=True):
+def refine_nyms(nyms_ls, target_word, exclude_multiword=True):
+    oneword_nyms = []
+
     space_characters = ['_', ' ']
-    final_synonyms = [s for s in synonyms_ls if s != target_word]
+    nyms_ls = [s for s in nyms_ls if s != target_word]
+
 
     if exclude_multiword:
-        for syn in synonyms_ls:
+        for syn in nyms_ls:
             oneword_flag = True
             for c in space_characters:
                 if c in syn:
                     oneword_flag = False
             if oneword_flag:
-                final_synonyms.append(syn)
+                oneword_nyms.append(syn)
 
+    final_nyms = [oneword_nyms[i] for i in range(len(oneword_nyms)) if oneword_nyms[i] not in oneword_nyms[i+1:]]
 
-    return final_synonyms
+    return final_nyms
 
 
 def getAndSave_inputData(vocabulary=[]):
@@ -35,7 +41,7 @@ def getAndSave_inputData(vocabulary=[]):
     if not(os.path.exists(Utils.FOLDER_RAW_INPUT)):
         os.mkdir(Utils.FOLDER_RAW_INPUT)
 
-    toy_vocabulary = ['light']#["wide", "plant", "sea", "high", "move"]
+    toy_vocabulary = ['light', 'low']#["wide", "plant", "sea", "high", "move"]
     vocabulary = toy_vocabulary
 
 
@@ -43,46 +49,51 @@ def getAndSave_inputData(vocabulary=[]):
     # tables in the HDF5 storage is sorted.
     vocabulary_sorted = sorted(vocabulary)
 
-    tasks = [WordNet.retrieve_DESA, Wiktionary.retrieve_DE, OmegaWiki.retrieve_DS,
+    tasks = [WordNet.retrieve_DESA, Wiktionary.retrieve_DESA, OmegaWiki.retrieve_DS,
              DBpedia.retrieve_dbpedia_def, BabelNet.retrieve_DES]
     sources = [Utils.SOURCE_WORDNET, Utils.SOURCE_WIKTIONARY, Utils.SOURCE_OMEGAWIKI, Utils.SOURCE_DBPEDIA, Utils.SOURCE_BABELNET]
 
     categories = [Utils.DEFINITIONS, Utils.EXAMPLES, Utils.SYNONYMS, Utils.ANTONYMS, Utils.ENCYCLOPEDIA_DEF]
-    hdf5_min_itemsizes_dict = {Utils.DEFINITIONS:HDF5_BASE_CHARSIZE, Utils.EXAMPLES:HDF5_BASE_CHARSIZE,
-                               Utils.SYNONYMS:HDF5_BASE_CHARSIZE, Utils.ANTONYMS:HDF5_BASE_CHARSIZE,
+    hdf5_min_itemsizes_dict = {'word': HDF5_BASE_CHARSIZE/4, 'source':HDF5_BASE_CHARSIZE/4,
+                               Utils.DEFINITIONS:HDF5_BASE_CHARSIZE, Utils.EXAMPLES:HDF5_BASE_CHARSIZE,
+                               Utils.SYNONYMS:HDF5_BASE_CHARSIZE/2, Utils.ANTONYMS:HDF5_BASE_CHARSIZE/2,
                                Utils.ENCYCLOPEDIA_DEF:4*HDF5_BASE_CHARSIZE}
 
     storage_filenames = [Utils.H5_raw_defs, Utils.H5_examples, Utils.H5_synonyms, Utils.H5_antonyms, Utils.H5_enc_defs]
     storage_filepaths = list(map(lambda fn: os.path.join(Utils.FOLDER_RAW_INPUT, fn) , storage_filenames))
     open_storage_files = [ pd.HDFStore(fname, mode='w') for fname in storage_filepaths] #reset HDF5 archives
 
-    categories_returned = [[0,1,2,3],[0,1],[0,2],[4], [0,1,2]]
-    num_columns = [4,2,2,1, 3]
+    categories_returned = [[0,1,2,3],[0,1,2,3],[0,2],[4], [0,1,2]]
+    num_columns = [4,4,2,1, 3]
 
-    for i in range(len(tasks)):
+    for i in range(len(tasks)-1):
         task = tasks[i]
         source = sources[i]
         cats = categories_returned[i]
         n_cols = num_columns[i]
+        task_runtime = 0
 
         for word in vocabulary_sorted:
             task_time_start = time.clock()
             desa = task(word)
-            task_runtime = time.clock() - task_time_start
-            logging.info("Word: " + word + " ; source: " + source + " ; task runtime = " + str(round(task_runtime,5)))
+            task_runtime = task_runtime + (time.clock() - task_time_start)
             logging.debug(desa)
             for j in range(n_cols):
                 current_column = categories[cats[j]]
                 logging.info(current_column)
-                # Post-process if needed. No multi-word synonyms for now
-                if current_column == Utils.SYNONYMS:
-                    column_data = refine_synonyms(desa[j], word)
+                # Post-process: No target word. No duplicates. No multi-word synonyms for now
+                if current_column in [Utils.SYNONYMS, Utils.ANTONYMS]:
+                    column_data = refine_nyms(desa[j], word)
                 else:
                     column_data = desa[j]
-                df = pd.DataFrame(data=column_data, columns=[current_column])
+                df_data = zip( cycle([word]), column_data, cycle([source]))
+                df_columns = ['word',current_column,'source']
+                df = pd.DataFrame(data=df_data, columns=df_columns)
 
                 (open_storage_files[cats[j]]).append(key=current_column, value=df,
-                                                     min_itemsize={key:hdf5_min_itemsizes_dict[key] for key in [current_column]})
+                                                     min_itemsize={key:hdf5_min_itemsizes_dict[key] for key in df_columns})
+
+        logging.info("Source: " + source + " ; task runtime = " + str(round(task_runtime, 5)))
 
     for storagefile in open_storage_files:
         storagefile.close()
--- GetInputData/Wiktionary.py
+++ GetInputData/Wiktionary.py
@@ -5,34 +5,52 @@ import re
 
 #URL = "https://en.wiktionary.org/w/api.php"
 
+def extract_synonyms_or_antonyms(relatedWords_dict):
+    nyms_lls = relatedWords_dict['words']
+    nyms_lls = clear_grammar_in_brackets(nyms_lls)
+    nyms_lls_clean = list(map(lambda s: s[2:], nyms_lls))  # skip ' :'
+    nyms_string = ','.join(nyms_lls_clean)
+    nyms = nyms_string.split(sep=',')
+    return nyms
+
+
 def process_defs_subdicts(structure_ls):
 
     defs = []
     examples = []
+    synonyms = []
+    antonyms = []
 
     for pos_dict in structure_ls:
         defs.extend(pos_dict['text'][1:]) # skip [0] as it is grammar, not a definition
         examples.extend(pos_dict['examples'])
+        related_words_dictsls = pos_dict['relatedWords']
+        for d in related_words_dictsls:
+            if d['relationshipType'] == 'synonyms':
+                synonyms = extract_synonyms_or_antonyms(d)
+            if d['relationshipType'] == 'antonyms':
+                antonyms = extract_synonyms_or_antonyms(d)
 
-    return (defs, examples)
+
+    return (defs, examples, synonyms, antonyms)
 
 
 # Wiktionary definition often start with a specification, like (transitive), (obsolete), (botany), (uncountable).
 # It should be removed.
-def clean_definitions(defs_ls):
+def clear_grammar_in_brackets(defs_ls):
     new_defs = list(map(lambda definition: re.sub("^\([a-zA-Z0-9_\s,]+\)" , "", definition) , defs_ls))
     return new_defs
 
 
-def retrieve_DE(target_word):
+def retrieve_DESA(target_word):
     parser = WP.WiktionaryParser()
     word_data = parser.fetch(target_word)
 
     defs_structure = word_data[0]['definitions']
-    (raw_definitions, examples) = process_defs_subdicts(defs_structure)
-    definitions = clean_definitions(raw_definitions)
+    (raw_definitions, examples, synonyms, antonyms) = process_defs_subdicts(defs_structure)
+    definitions = clear_grammar_in_brackets(raw_definitions)
 
-    return definitions, examples
+    return definitions, examples, synonyms, antonyms
 
 
 def main():
@@ -40,7 +58,7 @@ def main():
 
     target_word = "plant"
 
-    definitions, _examples = retrieve_DE(target_word)
+    definitions, _examples, _synonyms, _antonyms = retrieve_DESA(target_word)
     logging.info(definitions)
 
 
