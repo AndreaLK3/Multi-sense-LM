08858a8d8cbbf916a4c7633fc082cf1166de3f24
Andrea Lekkas <anl@ordbogen.com>
2019-08-25 14:41:01 +0200

    Proceeding to: implementing standard Skip-Gram, starting from pretrained embeddings
    Former-commit-id: e572f466b1057db84c39126c3f6c6c31329efd3d

--- CreateEntities/Examples.py
+++ CreateEntities/Examples.py
@@ -4,33 +4,47 @@ import os
 import logging
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+import nltk
 
+CHUNKSIZE_HDF5 = 128
 
 def main():
     Utils.init_logging(os.path.join("CreateEntities", "Examples.log"), logging.INFO)
 
     # Load vectors directly from the file
-    model = gensim.models.KeyedVectors.load_word2vec_format(os.path.join(Utils.FOLDER_WORD_EMBEDDINGS, Utils.WORD2VEC_FILENAME), binary=True)
-    # Access vectors for specific words with a keyed lookup:
-    vector = model['easy']
-    # see the shape of the vector (300,)
-    logging.info(vector.shape)
-    logging.info(vector[0:10])
+    model_wv = gensim.models.KeyedVectors.load_word2vec_format(os.path.join(Utils.FOLDER_WORD_EMBEDDINGS, Utils.WORD2VEC_FILENAME), binary=True)
 
+    # The vectors and vocabulary can be found (and zipped, if necessary) at: model_wv.vectors, model_wv.index2word
+
+    vocab_size = len(model_wv.vectors)  # Number of unique words in our corpus of text ( Vocabulary )
+    d = len(model_wv.vectors[0])  # 300   # Number of neurons in the hidden layer of neural network
+
+    target_words = ["wide"] # retrieve the updated vectors for those at the end.
+    all_examples_df_chunksIter = pd.read_hdf(os.path.join(Utils.FOLDER_INPUT, Utils.PREP_EXAMPLES + ".h5")
+                                  ,iterator=True, chunksize=CHUNKSIZE_HDF5)
+
+    sentences_tokenized_lls = []
+    for chunk in all_examples_df_chunksIter:
+        chunk_lls = []
+        for row in chunk.itertuples():
+            chunk_lls.append(nltk.tokenize.word_tokenize(row.examples))
+        sentences_tokenized_lls.extend(chunk_lls)
+
+
+    # Replace the target word's embedding with a random vector
+    # matrix_embeddings = model_wv.vectors
+    # matrix_embeddings[center_word_embedding_index] = np.random.normal(loc=0, scale=1.0, size=d)
+
+    # Concatenate the example_entity embedding for our center word, randomly initialized. Its index will be |V|+1
+    #model_wv.add(entities=["EXAMPLES_"+center_word], weights=np.random.normal(loc=0, scale=1.0, size=d), replace=False)
 
 
     # In skip gram architecture of word2vec, the input is the center word and the predictions are the context words.
     # Consider an array of words W, if W(i) is the input (center word), then W(i-2), W(i-1), W(i+1), and W(i+2) are the
     # context words, if the sliding window size is 2.
 
-    # Defining some variables :
 
-    V = 0 # Number of unique words in our corpus of text ( Vocabulary )
-    x = 0  # Input layer (One hot encoding of our input word ).
-    d = 300   # Number of neurons in the hidden layer of neural network
-    #W = V * d   # Weights between input layer and hidden layer (V x N)
-    #W_prime = d * V  # Weights between hidden layer and output layer (N x V)
-    y =0   # A softmax output layer having probabilities of every word in our vocabulary
 
 
     # If I am using the pretrained embeddings... I should use the weight matrices from the loaded model,
@@ -38,31 +52,27 @@ def main():
     # note: instead of using the entire matrix, I could extract a meaningful vocabulary, e.g. with 27K instead of 3M words
 
     # Part of it from: https://github.com/vyomshm/Skip-gram-Word2vec/blob/master/Skip-Gram%2Bword2vec.ipynb
+    # and from: https://tensorflow.org/tutorials/representation/word2vec
 
     nn_graph = tf.Graph()
     with nn_graph.as_default():
         center_word_idx = tf.placeholder(dtype=tf.int32, name="input_pl") # vocab. index for the 1-hot encoding input
-        target_word_idx = tf.placeholder(dtype=tf.int32, name="label_pl")
+        word_toPredict_idx = tf.placeholder(dtype=tf.int32, name="label_pl")
 
-        W_E = tf.get_variable(name="embeddings_fixed",shape=(V, d), initializer=tf.random_uniform([V,d],-1, 1))
-        W_e_target = tf.get_variable(name="embedding_target",shape=(1, d), initializer=tf.random_uniform([V,d],-1, 1))
+        W_E = tf.get_variable(name="embeddings",shape=(vocab_size, d), initializer=model_wv.vectors)
 
         embed = tf.nn.embedding_lookup(W_E,[center_word_idx])
 
         #hidden_layer = tf.matmul(center_word, W_E)
 
-        W_h_out = tf.get_variable(name="weights_h_out", shape=(d, V), initializer=tf.contrib.layers.xavier_initializer())
-        biases_out = tf.get_variable(name='biases_out', shape=d,
-                                   initializer=tf.contrib.layers.xavier_initializer())  # tf.zeros([hidden1_units])
-
-        logits = tf.matmul(embed, W_h_out) + biases_out
-        softmax_b = tf.Variable(tf.zeros(V))
+        W_h_out = tf.get_variable(name="weights_h_out", shape=(d, vocab_size), initializer=tf.contrib.layers.xavier_initializer())
+        biases_out = tf.get_variable(name='biases_out', shape=vocab_size,
+                                   initializer=tf.zeros([vocab_size]))  #
 
         # Calculate the loss using negative sampling
-        loss = tf.nn.sampled_softmax_loss(logits, softmax_b, target_word_idx, embed, num_sampled=5, num_classes=V)
-        tvars = tf.trainable_variables()
+        loss = tf.nn.sampled_softmax_loss(W_h_out, biases_out, word_toPredict_idx, embed, num_sampled=10, num_classes=vocab_size)
+        # tvars = tf.trainable_variables()
+        # vars_to_change = [var for var in tvars if not('fixed' in var.name)]
 
-        vars_to_change = [var for var in tvars if not('fixed' in var.name)]
-
-        optimizer = tf.train.AdamOptimizer().minimize(loss, var_list=vars_to_change)
+        optimizer = tf.train.AdamOptimizer().minimize(loss)
 
