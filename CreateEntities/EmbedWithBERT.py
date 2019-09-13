746a82807517c3efb3556d129bf85fa04c488b1e
Andrea Lekkas <anl@ordbogen.com>
2019-09-13 10:34:04 +0200

    =Added BERT sentence embedding. About to modify the Preprocessing phase...
    Former-commit-id: 9099af215b746cf96386d3ab5a17344cf14d14e8

--- CreateEntities/EmbedWithBERT.py
+++ CreateEntities/EmbedWithBERT.py
@@ -7,37 +7,40 @@ START_SENTENCE_TOKEN = "[CLS]"
 END_SEP_TOKEN = "[SEP]"
 
 
-def get_sentence_embedding(model, sentence_text):
-    Utils.init_logging("EmbedWithBERT.log")
+def get_sentence_embedding(model, tokenizer, sentence_text):
 
-    tokenizer = pt.BertTokenizer.from_pretrained('bert-base-uncased')
-
-    toks_ = tokenizer.tokenize(START_SENTENCE_TOKEN + sentence_text + END_SEP_TOKEN)
-    indices = tokenizer.convert_tokens_to_ids(toks_)
+    toks = tokenizer.tokenize(START_SENTENCE_TOKEN + sentence_text + END_SEP_TOKEN)
+    indices = tokenizer.convert_tokens_to_ids(toks)
 
     segment_ids = [1] * len(indices)# single-sentence inputs only require a series of 1s
 
-    logging.info(toks_)
+    logging.info(toks)
 
     # Convert inputs to PyTorch tensors
-    tokens_tensor = torch.tensor(indices)
-    segment_tensor = torch.tensor(segment_ids)
+    tokens_tensor = torch.tensor(indices).unsqueeze(0)
+    segment_tensor = torch.tensor(segment_ids).unsqueeze(0)
 
     with torch.no_grad():
         last_layer = model(tokens_tensor, segment_tensor)[0]
-        # last_hidden_state: torch.FloatTensor of shape (batch_size, sequence_length, hidden_size)
+        # last_hidden_state: torch.FloatTensor of shape (batch_size, sequence_length, hidden_size) (here [1, 17, 768])
 
     #  The correct pooling strategy (mean, max, concatenation, etc.) and layers used (last four, all, last layer, etc.)
     #  are dependent on the application
 
     # To get a single vector for our entire sentence we have multiple application-dependent strategies,
-    # a simple approach is to average the second-to-last hidden layer of each token, producing one 768-length vector
-    sentence_embedding = torch.mean(last_layer[0][:][:], 1)
+    # a simple approach is to average the (/second-to-last) hidden layer of each token, producing one 768-length vector
+
+
+    sentence_embedding = torch.mean(last_layer, dim=1)[0] # batch size 1
+    logging.info(sentence_embedding.shape)
 
     return sentence_embedding
 
 
-
-model = pt.BertModel.from_pretrained('bert-base-uncased')
-example_sentence = "Dictionary definitions and other elements must be converted into sentence embeddings."
-get_sentence_embedding(model, example_sentence)
+def main():
+    Utils.init_logging("EmbedWithBERT.log")
+    model = pt.BertModel.from_pretrained('bert-base-uncased')
+    tokenizer = pt.BertTokenizer.from_pretrained('bert-base-uncased')
+    example_sentence = "Dictionary definitions and other elements must be converted into sentence embeddings."
+    sent_vec = get_sentence_embedding(model, tokenizer, example_sentence)
+    return sent_vec
