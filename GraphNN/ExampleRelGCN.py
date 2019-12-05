6ff5557d500c4ff4b3f1bb92c525f25444f90b72
Andrea Lekkas <anl@ordbogen.com>
2019-12-05 14:27:00 +0100

    =The RCGN ; proceeding to try to send as input the entire graph-data object to the forward(...) call ...
    Former-commit-id: 4d3c8330e927eeb0ef343d25899ff53496ad2524
    Former-commit-id: ebf54d2896e45d5286ef08fd1586bc328cc02301

--- GraphNN/ExampleRelGCN.py
+++ GraphNN/ExampleRelGCN.py
@@ -1,7 +1,7 @@
 import torch
 import torch_geometric
 from torch_geometric.nn import RGCNConv
-import torch.nn.functional as F
+import torch.nn.functional as tF
 from WordEmbeddings.ComputeEmbeddings import Method
 import os
 import random
@@ -13,6 +13,92 @@ import networkx
 
 # In this example, we do not extract the node features, word and sense vocabulary indices, etc.
 # We use Random Number Generation to create a small structure.
+
+def createInputGraph():
+
+    num_senses = 10
+    num_sp = 5
+    num_def = 20
+    num_exs = 20
+    NUM_NODES = num_senses + num_sp + num_def + num_exs
+    NUM_NODE_FEATURES = 10
+    NUM_RELATIONS = 5
+    # X (Tensor, optional) – Node feature matrix with shape [num_nodes, num_node_features]. (default: None)
+    #
+    # The nodes are: all the sense embeddings + all the single-prototype embeddings
+    #               + all the sentence embeddings from the definitions and examples.
+    # All the nodes must have an index, from 0 to num_nodes -1, and the same dimensionality, num_node_features.
+    X_senses = torch.rand((num_senses, NUM_NODE_FEATURES))
+    X_sp = torch.rand((num_sp, NUM_NODE_FEATURES))
+    X_defs = torch.rand((num_def, NUM_NODE_FEATURES))
+    X_exs = torch.rand((num_exs, NUM_NODE_FEATURES))
+
+    # The order for the index of the nodes?
+    # sense = [0,se) ; single prototype = [se,se+sp) ; definitions = [se+sp, se+sp+d) ; examples = [se+sp+d, e==num_nodes)
+    # The index will be the row number in the matrix X
+
+    # edge_index (LongTensor, optional) – Graph connectivity in COO format with shape [2, num_edges]. (default: None)
+    # We can operate with a list of S-D tuples, adding t().contiguous()
+    # The Procedure to set up both X and edge_index will be:
+    # - read the archive of processed definitions. We encounter the sense_wn_id (e.g. active.n.03), and the def. text
+    # - consider the database indices_table.sql. It has the columns: word_sense, vocab_index (from 0 to se),
+    #   start_defs, end_defs, start_examples, end_examples.
+    # - load (for instance) vectorized_FastText_definitions.npy. Even with > 20K vectors, it is very light at ~58MB
+    # - (Using WordNet as a source, we always have 1 definition). Use [start_defs, end_defs) to extract the vector(s) for
+    #   the definition of a sense, and append that vector to X_defs
+    # - Register the connections: add a tuple (source=definition_index=sp+def, target=sense)
+    # - Do the same for examples: get the examples' vectors for that sense from [start_examples, end_examples) in
+    #   indices_table.sql, and get the corresponding rows of vectorized_FastText_examples.npy.
+    # - Append vectors to submatrix X_exs (to be put together later), and add the connections [d, e==num_nodes) ->[0,se)
+
+    logging.info("Senses : [ " + str(0) + " , " + str(num_senses) + ")")
+    logging.info("Global single-prototypes : [ " + str(num_senses) + " , " + str(num_senses + num_sp) + ")")
+    logging.info("Definitions : [ " + str(num_senses + num_sp) + " , " + str(num_senses + num_sp + num_def) + ")")
+    logging.info("Examples : [ " + str(num_senses + num_sp + num_def) + " , " + str(NUM_NODES) + ")")
+
+
+    # For this example, however, we initialized randomly the features of the vectors, and we proceed by
+    # randomly determining connections between different kinds of nodes, in the way the task allows.
+    # definitions -> senses : [se+sp, se+sp+d) -> [0,se)
+    edges_defs = setrandomedges_d_or_e(0, num_senses, num_senses+num_sp, num_senses+num_sp+num_def)
+    logging.debug("Edges - def . Connections:" + str(edges_defs))
+
+    # examples --> senses : [se+sp+d, e==num_nodes) -> [0,se)
+    edges_exs = setrandomedges_d_or_e(0, num_senses, num_senses+num_sp+num_def, NUM_NODES)
+    logging.debug("Edges - exs . Connections:" + str(edges_exs))
+
+    # global (a.k.a. single-prototype) -> senses : [se,se+sp) -> [0,se)
+    edges_sc = setrandomedges_sc(0, num_senses, num_senses, num_senses+num_sp)
+    logging.debug("Edges - sc . Connections:" + str(edges_sc))
+
+    # global -> global : [se,se+sp) -> [se,se+sp). Bidirectional (which means 2 connections, (a,b) and (b,a)
+    edges_syn = setrandomedges_syn_or_ant(num_senses, num_senses+num_sp)
+    logging.debug("Edges - syn . Connections:" + str(edges_syn))
+    edges_ant = setrandomedges_syn_or_ant(num_senses, num_senses+num_sp,
+                                                     restrict=True, opposite_connections_lts=edges_syn)
+    logging.debug("Edges - ant . Connections:" + str(edges_ant))
+
+
+    ##### Reuniting the components of the graph
+    X = torch.cat([X_senses, X_sp, X_defs, X_exs])
+    all_edges_lts = torch.tensor(edges_defs + edges_exs + edges_sc + edges_syn + edges_ant)
+    edge_types = torch.tensor([0] * len(edges_defs) + [1] * len(edges_exs) + [2] * len(edges_sc) +
+                              [3] * len(edges_syn) + [4] * len(edges_ant))
+    node_types = torch.tensor([0]*num_senses + [1]*num_sp + [2]*num_def + [3]*num_exs)
+
+    all_edges = all_edges_lts.t().contiguous()
+    graph = torch_geometric.data.Data(x=X,
+                                      edge_index=all_edges,
+                                      edge_type=edge_types,
+                                      node_types=node_types,
+                                      num_relations=NUM_RELATIONS)
+
+    ##### Printing
+    #netx_graph = convert.to_networkx(graph)
+    #networkx.write_gexf(netx_graph, path=os.path.join('GraphNN', 'exampleGraph.gexf'))
+
+    return graph
+
 
 # Utility function
 def convert_dict_into_lts(dict_target_sources):
@@ -70,110 +156,83 @@ def setrandomedges_syn_or_ant(global_low, global_high, restrict=False, opposite_
 ######
 
 ##### The RGCN
-# Modified from: https://github.com/rusty1s/pytorch_geometric/blob/master/examples/rgcn.py
+# From the GCN example at: https://pytorch-geometric.readthedocs.io/en/latest/notes/introduction.html
+# class Net(torch.nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = GCNConv(dataset.num_node_features, 16)
+#         self.conv2 = GCNConv(16, dataset.num_classes)
+#
+#     def forward(self, data):
+#         x, edge_index = data.x, data.edge_index
+#
+#         x = self.conv1(x, edge_index)
+#         x = F.relu(x)
+#         x = F.dropout(x, training=self.training)
+#         x = self.conv2(x, edge_index)
+#
+#         return F.log_softmax(x, dim=1)
+# From the example at: https://github.com/rusty1s/pytorch_geometric/blob/master/examples/rgcn.py
+# class Net(torch.nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = RGCNConv(
+#             data.num_nodes, 16, dataset.num_relations, num_bases=30)
+#         self.conv2 = RGCNConv(
+#             16, dataset.num_classes, dataset.num_relations, num_bases=30)
+#
+#     def forward(self, edge_index, edge_type, edge_norm):
+#         x = F.relu(self.conv1(None, edge_index, edge_type))
+#         x = self.conv2(x, edge_index, edge_type)
+#         return F.log_softmax(x, dim=1)
 class NetRGCN(torch.nn.Module):
     def __init__(self, data):
         super(NetRGCN, self).__init__()
-        # self.conv1 = RGCNConv(
-        #     data.num_nodes, 16, data.num_relations, num_bases=data.num_relations)
-        # self.conv2 = RGCNConv(
-        #     16, dataset.num_classes, data.num_relations, num_bases=data.num_relations)
-        self.conv1 = RGCNConv(in_channels=data.x.shape[0], # number of rows in X
-                              out_channels=data.x.shape[0], num_relations=data.num_relations, num_bases=data.num_relations)
-        self.senses_to_global_indexpoint = data.node_types.tolist.index(1)
+        self.conv1 = RGCNConv(in_channels=data.x.shape[1], # the number of features of a node
+                              out_channels=data.x.shape[1], # representation at layer L+1
+                              num_relations=data.num_relations, num_bases=data.num_relations)
+        self.last_idx_senses = data.node_types.tolist().index(1)
+        self.last_idx_globals = data.node_types.tolist().index(2)
+        self.conv2global = RGCNConv(in_channels=data.x.shape[1],  # the number of features of a node
+                                    out_channels=self.last_idx_globals,  # the number of global/sp nodes, to predict one of them
+                                    num_relations=data.num_relations, num_bases=data.num_relations)
+        self.conv2senses = RGCNConv(in_channels=data.x.shape[1],  # the number of features of a node
+                                    out_channels=self.last_idx_senses,  # the number of senses nodes, to predict one of them
+                                    num_relations=data.num_relations, num_bases=data.num_relations)
 
-    def forward(self, edge_index, edge_type, edge_norm):
-        # x = F.relu(self.conv1(None, edge_index, edge_type))
-        # x = self.conv2(x, edge_index, edge_type)
-        # return F.log_softmax(x, dim=1)
-        x = self.conv1(None, edge_index, edge_type)
+    # observation: the RGCNConv's forward has the signature: forward(x, edge_index, edge_type, edge_norm=None, size=None)
+    def forward(self, node_vector, edge_index, edge_type):
+        x = tF.relu(self.conv1(node_vector, edge_index, edge_type))
+        logits_globals = self.conv2global(x, edge_index, edge_type)
+        logits_senses = self.conv2senses(x, edge_index, edge_type)
+        return (tF.log_softmax(logits_globals, dim=1), tF.log_softmax(logits_senses, dim=1))
 
 
-
-
-def createInputGraph():
+def train():
     Utils.init_logging('temp.log')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    inputgraph_dataobject = createInputGraph()
+    RGCN_modelobject = NetRGCN(inputgraph_dataobject)
 
-    num_senses = 4
-    num_sp = 2
-    num_def = 8
-    num_exs = 8
-    NUM_NODES = num_senses + num_sp + num_def + num_exs
-    NUM_NODE_FEATURES = 10
-    NUM_RELATIONS = 5
-    # X (Tensor, optional) – Node feature matrix with shape [num_nodes, num_node_features]. (default: None)
-    #
-    # The nodes are: all the sense embeddings + all the single-prototype embeddings
-    #               + all the sentence embeddings from the definitions and examples.
-    # All the nodes must have an index, from 0 to num_nodes -1, and the same dimensionality, num_node_features.
-    X_senses = torch.rand((num_senses, NUM_NODE_FEATURES))
-    X_sp = torch.rand((num_sp, NUM_NODE_FEATURES))
-    X_defs = torch.rand((num_def, NUM_NODE_FEATURES))
-    X_exs = torch.rand((num_exs, NUM_NODE_FEATURES))
+    data, model = inputgraph_dataobject.to(device), RGCN_modelobject.to(device)
+    model.train()
 
-    # The order for the index of the nodes?
-    # sense = [0,se) ; single prototype = [se,se+sp) ; definitions = [se+sp, se+sp+d) ; examples = [se+sp+d, e==num_nodes)
-    # The index will be the row number in the matrix X
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0005)
+    optimizer.zero_grad()
 
-    # edge_index (LongTensor, optional) – Graph connectivity in COO format with shape [2, num_edges]. (default: None)
-    # We can operate with a list of S-D tuples, adding t().contiguous()
-    # The Procedure to set up both X and edge_index will be:
-    # - read the archive of processed definitions. We encounter the sense_wn_id (e.g. active.n.03), and the def. text
-    # - consider the database indices_table.sql. It has the columns: word_sense, vocab_index (from 0 to se),
-    #   start_defs, end_defs, start_examples, end_examples.
-    # - load (for instance) vectorized_FastText_definitions.npy. Even with > 20K vectors, it is very light at ~58MB
-    # - (Using WordNet as a source, we always have 1 definition). Use [start_defs, end_defs) to extract the vector(s) for
-    #   the definition of a sense, and append that vector to X_defs
-    # - Register the connections: add a tuple (source=definition_index=sp+def, target=sense)
-    # - Do the same for examples: get the examples' vectors for that sense from [start_examples, end_examples) in
-    #   indices_table.sql, and get the corresponding rows of vectorized_FastText_examples.npy.
-    # - Append vectors to submatrix X_exs (to be put together later), and add the connections [d, e==num_nodes) ->[0,se)
+    #out = model(data.edge_index, data.edge_type, data.edge_norm)
+    training_dataset_length = 20
+    training_dataset = [random.randint(RGCN_modelobject.last_idx_senses, RGCN_modelobject.last_idx_globals-1)
+                        for i in range(training_dataset_length)] # only globals for now
+    logging.info("Training dataset = " + str(training_dataset))
 
-    logging.info("Senses : [ " + str(0) + " , " + str(num_senses) + ")")
-    logging.info("Global single-prototypes : [ " + str(num_senses) + " , " + str(num_senses + num_sp) + ")")
-    logging.info("Definitions : [ " + str(num_senses + num_sp) + " , " + str(num_senses + num_sp + num_def) + ")")
-    logging.info("Examples : [ " + str(num_senses + num_sp + num_def) + " , " + str(NUM_NODES) + ")")
+    for train_idx in training_dataset:
+        logging.info(data.x.shape)
+        predicted_global, predicted_sense = model(data.x[train_idx], data.edge_index[train_idx], data.edge_type[train_idx])
+        tF.nll_loss(predicted_global, training_dataset[train_idx+1]).backward()
+    optimizer.step()
 
 
-    # For this example, however, we initialized randomly the features of the vectors, and we proceed by
-    # randomly determining connections between different kinds of nodes, in the way the task allows.
-    # definitions -> senses : [se+sp, se+sp+d) -> [0,se)
-    edges_defs = setrandomedges_d_or_e(0, num_senses, num_senses+num_sp, num_senses+num_sp+num_def)
-    logging.info("Edges - def . Connections:" + str(edges_defs))
-
-    # examples --> senses : [se+sp+d, e==num_nodes) -> [0,se)
-    edges_exs = setrandomedges_d_or_e(0, num_senses, num_senses+num_sp+num_def, NUM_NODES)
-    logging.info("Edges - exs . Connections:" + str(edges_exs))
-
-    # global (a.k.a. single-prototype) -> senses : [se,se+sp) -> [0,se)
-    edges_sc = setrandomedges_sc(0, num_senses, num_senses, num_senses+num_sp)
-    logging.info("Edges - sc . Connections:" + str(edges_sc))
-
-    # global -> global : [se,se+sp) -> [se,se+sp). Bidirectional (which means 2 connections, (a,b) and (b,a)
-    edges_syn = setrandomedges_syn_or_ant(num_senses, num_senses+num_sp)
-    logging.info("Edges - syn . Connections:" + str(edges_syn))
-    edges_ant = setrandomedges_syn_or_ant(num_senses, num_senses+num_sp,
-                                                     restrict=True, opposite_connections_lts=edges_syn)
-    logging.info("Edges - ant . Connections:" + str(edges_ant))
-
-
-    ##### Reuniting the components of the graph
-    X = torch.cat([X_senses, X_sp, X_defs, X_exs])
-    all_edges_lts = torch.tensor(edges_defs + edges_exs + edges_sc + edges_syn + edges_ant)
-    edge_types = torch.tensor([0] * len(edges_defs) + [1] * len(edges_exs) + [2] * len(edges_sc) +
-                              [3] * len(edges_syn) + [4] * len(edges_ant))
-    node_types = torch.tensor([0]*num_senses + [1]*num_sp + [2]*num_def + [3]*num_exs)
-
-    all_edges = all_edges_lts.t().contiguous()
-    graph = torch_geometric.data.Data(x=X,
-                                      edge_index=all_edges,
-                                      edge_type=edge_types,
-                                      node_types = node_types,
-                                      num_relations=NUM_RELATIONS)
-
-    ##### Printing
-    netx_graph = convert.to_networkx(graph)
-    networkx.write_gexf(netx_graph, path=os.path.join('GraphNN', 'exampleGraph.gexf'))
 
 
 
