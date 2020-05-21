=Fixed loss_sense computation by using ignore_index
Former-commit-id: f4647d687767f5a6c7848511144a715c0d5128e7 [formerly c07a844c60282b5f782d922ef5b2a538775214c5]
Former-commit-id: cd9b944d751d424fdbba1188f1c30d774052a518
Former-commit-id: 3d831806a922152b1a4133028b1bce2519531a03

GNN/Models/GRUs.py
@@ -40,7 +40,7 @@ class GRU_base2(torch.nn.Module):
 
         # This is overwritten at the 1st forward, when we know the distributed batch size
         self.memory_hn = Parameter(torch.zeros(size=(n_layers, batch_size, n_units)), requires_grad=False)
-        self.memory_hn_senses = Parameter(torch.zeros(size=(n_layers-1, batch_size, n_units)), requires_grad=False)
+        self.memory_hn_senses = Parameter(torch.zeros(size=(n_layers, batch_size, n_units)), requires_grad=False)
         self.hidden_state_bsize_adjusted = False
 
         # GRU for globals - standard Language Model
@@ -51,7 +51,7 @@ class GRU_base2(torch.nn.Module):
             self.gat_senses = GATConv(in_channels=self.d, out_channels=int(self.d/4), heads=4)
         # GRU for senses
         if predict_senses:
-            self.gru_senses = torch.nn.GRU(input_size=self.concatenated_input_dim, hidden_size=n_units, num_layers=n_layers-1)
+            self.gru_senses = torch.nn.GRU(input_size=self.concatenated_input_dim, hidden_size=n_units, num_layers=n_layers)
 
         # 2nd part of the network as before: 2 linear layers to the logits
         self.linear2global = torch.nn.Linear(in_features=n_units,
@@ -70,8 +70,8 @@ class GRU_base2(torch.nn.Module):
             self.memory_hn = Parameter(torch.reshape(self.memory_hn.flatten()[0:new_num_hidden_state_elems],
                                            (self.n_layers, distributed_batch_size, self.n_units)), requires_grad=False)
             self.memory_hn_senses = Parameter(
-                            torch.reshape(self.memory_hn_senses.flatten()[0:((self.n_layers-1) *distributed_batch_size * self.n_units)],
-                                          (self.n_layers-1, distributed_batch_size, self.n_units)),
+                            torch.reshape(self.memory_hn_senses.flatten()[0:((self.n_layers) *distributed_batch_size * self.n_units)],
+                                          (self.n_layers, distributed_batch_size, self.n_units)),
                             requires_grad=False)
             self.hidden_state_bsize_adjusted=True
         # T-BPTT: at the start of each batch, we detach_() the hidden state from the graph&history that created it
@@ -86,7 +86,7 @@ class GRU_base2(torch.nn.Module):
         batch_input_signals_ls = []
 
         for padded_sequence in sequences_in_the_batch_ls:
-            padded_sequence = padded_sequence.squeeze()
+            padded_sequence = padded_sequence.squeeze(dim=0)
             padded_sequence = padded_sequence.chunk(chunks=padded_sequence.shape[0], dim=0)
             sequence_lts = [unpack_input_tensor(sample_tensor, self.N) for sample_tensor in padded_sequence]
 
@@ -102,7 +102,7 @@ class GRU_base2(torch.nn.Module):
                     x_attention_state = self.gat_globals(x, edge_index_g)
                     currentglobal_node_state = x_attention_state.index_select(dim=0, index=self.select_first_indices[0].to(torch.int64))
                 else:
-                    currentglobal_node_state=None
+                    currentglobal_node_state = None
 
                 # Input signal n.3: the node-state of the current sense; + concatenating the input signals
                 if self.include_sensenode_input:

GNN/Models/Senses.py
@@ -117,9 +117,7 @@ class SelectK(torch.nn.Module):
                 if self.include_globalnode_input:
                     x = self.X.index_select(dim=0, index=x_indices_g.squeeze())
                     x_attention_state = self.gat_globals(x, edge_index_g)
-                    currentglobal_node_state = x_attention_state.index_select(dim=0,
-                                                                              index=self.select_first_indices[0].to(
-                                                                                  torch.int64))
+                    currentglobal_node_state = x_attention_state.index_select(dim=0,index=self.select_first_indices[0].to(torch.int64))
                 else:
                     currentglobal_node_state = None
 

GNN/Training.py
@@ -24,6 +24,8 @@ import GNN.Models.Senses as SensesNets
 from itertools import cycle
 import gc
 
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
 
 # Preliminary logging, to document the hyperparameters, the model, and its parameters #
 def write_doc_logging(train_dataloader, model, model_forParameters, learning_rate, num_epochs):
@@ -47,20 +49,21 @@ def write_doc_logging(train_dataloader, model, model_forParameters, learning_rat
 
 
 # Auxiliary function for compute_model_loss
-def compute_sense_loss(predictions_senses, batch_labels_senses):
-    batch_validsenses_predicted = []
-    batch_validsenses_labels = []
-    for i in range(batch_labels_senses.shape[0]):
-        senselabel = batch_labels_senses[i]
-        if senselabel != -1:
-            batch_validsenses_labels.append(senselabel.item())
-            batch_validsenses_predicted.append(predictions_senses[i])
-    if len(batch_validsenses_labels) >= 1:
-        loss_sense = tfunc.nll_loss(torch.stack(batch_validsenses_predicted).to(DEVICE),
-                                    torch.tensor(batch_validsenses_labels, dtype=torch.int64).to(DEVICE))
-    else:
-        loss_sense = torch.tensor(0).to(DEVICE)
-    return loss_sense
+# def compute_sense_loss(predictions_senses, batch_labels_senses):
+#     batch_validsenses_predicted = []
+#     batch_validsenses_labels = []
+#     for i in range(batch_labels_senses.shape[0]):
+#         senselabel = batch_labels_senses[i]
+#         if senselabel != -1:
+#             batch_validsenses_labels.append(senselabel.item())
+#             batch_validsenses_predicted.append(predictions_senses[i])
+#     if len(batch_validsenses_labels) >= 1:
+#         loss_sense = tfunc.nll_loss(torch.stack(batch_validsenses_predicted).to(DEVICE),
+#                                     torch.tensor(batch_validsenses_labels, dtype=torch.int64).to(DEVICE))
+#     else:
+#         loss_sense = torch.tensor(0).to(DEVICE)
+#     return loss_sense
+
 
 ################
 
@@ -76,7 +79,7 @@ def compute_model_loss(model,batch_input, batch_labels, verbose=False):
 
     model_forParameters = model.module if torch.cuda.device_count() > 1 and model.__class__.__name__=="DataParallel" else model
     if model_forParameters.predict_senses:
-        loss_sense = compute_sense_loss(predictions_senses, batch_labels_senses)
+        loss_sense = tfunc.nll_loss(predictions_senses, batch_labels_senses, ignore_index=-1)
     else:
         loss_sense = torch.tensor(0)
 
@@ -104,8 +107,11 @@ def training_setup(slc_or_text_corpus, include_globalnode_input, include_senseno
     #                            batch_size, n_layers=3, n_units=1150)
     # Must still try the standard GRU and GRU_GAT with graph input on WikiText-2 - this time with the correct vocabulary
     # The original GRU architecture has been updated into the GRUbase2 model in Senses - I just have to specify that predict_senses=False
+    # torch.manual_seed(1) # for reproducibility while conducting mini-experiments
+    # if torch.cuda.is_available():
+    #     torch.cuda.manual_seed_all(1)
     model = GRUs.GRU_base2(graph_dataobj, grapharea_size, include_globalnode_input, include_sensenode_input, predict_senses,
-                           batch_size, n_layers=3, n_units=1150)
+                           batch_size, n_layers=3, n_units=600)
 
     # model= MyRNN.GRU(graph_dataobj, grapharea_size, include_senses=include_senses, batchs_size=batch_size, n_layers=3, n_units=1150)
     # MyGAT.GRU_GAT(graph_dataobj, grapharea_size, num_gat_heads=4, include_senses=include_senses,
@@ -203,16 +209,13 @@ def training_loop(model, learning_rate, train_dataloader, valid_dataloader, num_
                     loss = loss_global + loss_sense
                 else:
                     loss = loss_global
-                #torch.autograd.set_detect_anomaly(True)
+
                 loss.backward()
-                #In the current version, we allow for defs and examples to be moved
-                #last_embedding_to_update = model_forDataLoading.last_idx_senses + model_forDataLoading.last_idx_globals
-                #model_forDataLoading.X.grad.data[last_embedding_to_update:,:].fill_(0) # defs and examples should not change
+
                 optimizer.step()
                 overall_step = overall_step + 1
                 epoch_step = epoch_step + 1
-                #logging.info("Iteration time=")
-                #Utils.log_chronometer([t0,time()])
+
                 if overall_step % steps_logging == 0:
                     logging.info("Global step=" + str(overall_step) + "\t ; Iteration time=" + str(round(time()-t0,5)))
                     Utils.log_chronometer([t0, time()])